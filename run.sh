fd7be6dfd78e0f9a41d0578f29d44b32788ae623
Jacob Sanford <jsanford@unb.ca>
2016-01-18 19:29:16 -0400

    Replace boilerplate text in run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 while :
 do
   /app/r53dyndns.py -v -U $ROUTE53_IP_URL -R $ROUTE53_DOMAIN_A_RECORD
-  echo "Program has been running for $ROUTE53_UPDATE_FREQUENCY seconds..."
+  echo "Sleeping for $ROUTE53_UPDATE_FREQUENCY seconds..."
   sleep $ROUTE53_UPDATE_FREQUENCY
 done
